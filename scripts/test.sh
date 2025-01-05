9f2ef30cdde0ed7e24a620fb1893df7498d1e8dd
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-01-05 10:32:25 +0100

    Simplify

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,13 +2,8 @@
 
 set -o errexit -o nounset -o pipefail
 
-main() {
-  local -r script_folder="$(dirname "$(readlink --canonicalize "$0")")"
-  cd "$(dirname "${script_folder}")"
+cd -- "$(dirname -- "$0")/.."
 
-  docker run --entrypoint sh --rm --volume "${PWD}":/workdir \
-    evolutics/travel-kit:0.8.0 -c \
-    'git ls-files -z | xargs -0 travel-kit check --'
-}
-
-main "$@"
+docker run --entrypoint sh --rm --volume "${PWD}":/workdir \
+  evolutics/travel-kit:0.8.0 -c \
+  'git ls-files -z | xargs -0 travel-kit check --'
